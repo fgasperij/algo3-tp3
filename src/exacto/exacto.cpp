45b38bd2df0b736d909a9250f02a531d6fe0d3fe
Fernando Gasperi Jabalera <fjabalera@manas.com.ar>
2014-11-15 19:08:31 -0300

    Exacto con todas las podas

--- src/exacto/exacto.cpp
+++ src/exacto/exacto.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <list>
-#include <limits>
 
 using namespace std;
 
-void kpmp(vector<vector<float> > &adym, vector<list<int> > &partition, 
+void kpmp_sin_podas(vector<vector<float> > &adym, vector<list<int> > &partition, 
+    vector<list<int> > &min_partition, int node, int k, int max_k_used, 
+    float total_weight, float &min_weight);
+void kpmp_solo_k_subconjuntos(vector<vector<float> > &adym, vector<list<int> > &partition, 
+    vector<list<int> > &min_partition, int node, int k, int max_k_used, 
+    float total_weight, float &min_weight);
+void kpmp_peso_minimo(vector<vector<float> > &adym, vector<list<int> > &partition, 
+    vector<list<int> > &min_partition, int node, int k, int max_k_used, 
+    float total_weight, float &min_weight);
+void kpmp_poda_pesada(vector<vector<float> > &adym, vector<list<int> > &partition, 
+    vector<list<int> > &min_partition, int node, int k, int max_k_used, 
+    float total_weight, float &min_weight);
+void kpmp_poda_total(vector<vector<float> > &adym, vector<list<int> > &partition, 
     vector<list<int> > &min_partition, int node, int k, int max_k_used, 
     float total_weight, float &min_weight);
 
@@ -30,15 +41,19 @@ int main (int argc, char *argv[])
   float w;
   for(int i = 0; i < m; i++) {
     cin >> u >> v >> w;
-    adym[u - 1][v - 1] = w;
-    adym[v - 1][u - 1] = w;
+    adym[u][v] = w;
+    adym[v][u] = w;
   }
 
   vector<list<int> > partition(k, list<int>());
   vector<list<int> > min_partition(k, list<int>());
   partition[0].push_back(0);
   float min_weight = numeric_limits<float>::max();
-  kpmp(adym, partition, min_partition, 1, k, 0, 0, min_weight);
+  //kpmp_sin_podas(adym, partition, min_partition, 1, k, 0, 0, min_weight);
+  //kpmp_solo_k_subconjuntos(adym, partition, min_partition, 1, k, 0, 0, min_weight);
+  //kpmp_peso_minimo(adym, partition, min_partition, 1, k, 0, 0, min_weight);
+  //kpmp_poda_pesada(adym, partition, min_partition, 1, k, 0, 0, min_weight);
+  kpmp_poda_total(adym, partition, min_partition, 1, k, 0, 0, min_weight);
 
   vector<int> node_indexed_partition(n, -1);
   for (int i = 0; i < k; i++) {
@@ -48,19 +63,87 @@ int main (int argc, char *argv[])
   }
 
   for (int i = 0; i < n; i++) {
-    cout << node_indexed_partition[i] + 1 << ' ';
+    cout << node_indexed_partition[i] << ' ';
   }
   cout << endl;
 
   return 0;
 }
 
+int weight_in_subset(int node, const list<int> &subset, const vector<vector<float> > &adym)
+{
+  int weight = 0; 
+  for(list<int>::const_iterator it = subset.begin(); it != subset.end(); it++) {
+    weight += adym[node][*it];
+  }
+
+  return weight;
+}
 
 
-void kpmp(vector<vector<float> > &adym, vector<list<int> > &partition, 
+void kpmp_sin_podas(vector<vector<float> > &adym, vector<list<int> > &partition, 
     vector<list<int> > &min_partition, int node, int k, int max_k_used, 
     float total_weight, float &min_weight)
 {
+  // check if has added all nodes
+  if (node == adym.size()) {
+    if (total_weight < min_weight && max_k_used < k) {
+      min_partition = partition;
+      min_weight = total_weight;
+    }
+    return;
+  }
+
+  // try adding it to every non-empty subset
+  for (int i = 0; i <= max_k_used; i++) {
+    float added_weight = weight_in_subset(node, partition[i], adym);
+    partition[i].push_back(node);
+    kpmp_sin_podas(adym, partition, min_partition, node+1, k, max_k_used, 
+        total_weight+added_weight, min_weight);
+    partition[i].pop_back();
+  }
+  // try adding new partition
+  partition[max_k_used+1].push_back(node);
+  kpmp_sin_podas(adym, partition, min_partition, node+1, k, max_k_used+1, 
+      total_weight, min_weight);
+  partition[max_k_used+1].pop_back();
+}
+
+void kpmp_peso_minimo(vector<vector<float> > &adym, vector<list<int> > &partition, 
+    vector<list<int> > &min_partition, int node, int k, int max_k_used, 
+    float total_weight, float &min_weight)
+{
+  // check if has added all nodes
+  if (node == adym.size()) {
+    if (total_weight < min_weight && max_k_used < k) {
+      min_partition = partition;
+      min_weight = total_weight;
+    }
+    return;
+  }
+
+  // try adding it to every non-empty subset
+  for (int i = 0; i <= max_k_used; i++) {
+    float added_weight = weight_in_subset(node, partition[i], adym);
+    if (total_weight+added_weight < min_weight) {
+      partition[i].push_back(node);
+      kpmp_peso_minimo(adym, partition, min_partition, node+1, k, max_k_used, 
+          total_weight+added_weight, min_weight);
+      partition[i].pop_back();
+    }
+  }
+  // try adding new partition
+  partition[max_k_used+1].push_back(node);
+  kpmp_peso_minimo(adym, partition, min_partition, node+1, k, max_k_used+1, 
+      total_weight, min_weight);
+  partition[max_k_used+1].pop_back();
+}
+
+void kpmp_solo_k_subconjuntos(vector<vector<float> > &adym, vector<list<int> > &partition, 
+    vector<list<int> > &min_partition, int node, int k, int max_k_used, 
+    float total_weight, float &min_weight)
+{
+  // check if has added all nodes
   if (node == adym.size()) {
     if (total_weight < min_weight) {
       min_partition = partition;
@@ -68,14 +151,21 @@ void kpmp(vector<vector<float> > &adym, vector<list<int> > &partition,
     }
     return;
   }
-
-  for (int i = 0; i <= max_k_used; i++) {
-    float added_weight = 0;
-    for(list<int>::iterator it = partition[i].begin(); it != partition[i].end(); it++) {
-      added_weight += adym[node][*it];
+  // because max_k_used starts at 0
+  if (adym.size()-node == (k-1)-max_k_used) {
+    for (int i = 0; i < (k-1)-max_k_used; ++i) {
+      partition[max_k_used+i+1].push_back(node+i);
     }
+    kpmp_solo_k_subconjuntos(adym, partition, min_partition, adym.size(), k, k-1, 
+        total_weight, min_weight);
+    return;
+  }
+
+  // try adding it to every non-empty subset
+  for (int i = 0; i <= max_k_used; i++) {
+    float added_weight = weight_in_subset(node, partition[i], adym);
     partition[i].push_back(node);
-    kpmp(adym, partition, min_partition, node+1, k, max_k_used, 
+    kpmp_solo_k_subconjuntos(adym, partition, min_partition, node+1, k, max_k_used, 
         total_weight+added_weight, min_weight);
     partition[i].pop_back();
   }
@@ -83,7 +173,127 @@ void kpmp(vector<vector<float> > &adym, vector<list<int> > &partition,
   if (max_k_used < k-1) {
     // try adding new partition
     partition[max_k_used+1].push_back(node);
-    kpmp(adym, partition, min_partition, node+1, k, max_k_used+1, 
+    kpmp_solo_k_subconjuntos(adym, partition, min_partition, node+1, k, max_k_used+1, 
+        total_weight, min_weight);
+    partition[max_k_used+1].pop_back();
+  }
+}
+
+void kpmp_poda_pesada(vector<vector<float> > &adym, vector<list<int> > &partition, 
+    vector<list<int> > &min_partition, int node, int k, int max_k_used, 
+    float total_weight, float &min_weight)
+{
+  // check if has added all nodes
+  if (node == adym.size()) {
+    if (total_weight < min_weight) {
+      min_partition = partition;
+      min_weight = total_weight;
+    }
+    return;
+  }
+  // because max_k_used starts at 0
+  if (adym.size()-node == (k-1)-max_k_used) {
+    for (int i = 0; i < (k-1)-max_k_used; ++i) {
+      partition[max_k_used+i+1].push_back(node+i);
+    }
+    kpmp_poda_pesada(adym, partition, min_partition, adym.size(), k, k-1, 
+        total_weight, min_weight);
+    return;
+  }
+
+  // poda pesada
+  if (max_k_used == k-1) {
+    int min_weight_of_adding_rest = 0;
+    for (int i = node; i < adym.size(); i++) {
+      float min_weight_in_subset = numeric_limits<float>::max();
+      for (int j = 0; j < k; j++) {
+        float current_weight = weight_in_subset(i, partition[j], adym);
+        if (current_weight < min_weight_in_subset) {
+          min_weight_in_subset = current_weight;
+        }
+      }
+      min_weight_of_adding_rest += min_weight_in_subset;
+    }
+
+    if (min_weight_of_adding_rest+total_weight >= min_weight) {
+      return;
+    }
+  }
+
+  // try adding it to every non-empty subset
+  for (int i = 0; i <= max_k_used; i++) {
+    float added_weight = weight_in_subset(node, partition[i], adym);
+    partition[i].push_back(node);
+    kpmp_poda_pesada(adym, partition, min_partition, node+1, k, max_k_used, 
+        total_weight+added_weight, min_weight);
+    partition[i].pop_back();
+  }
+
+  if (max_k_used < k-1) {
+    // try adding new partition
+    partition[max_k_used+1].push_back(node);
+    kpmp_poda_pesada(adym, partition, min_partition, node+1, k, max_k_used+1, 
+        total_weight, min_weight);
+    partition[max_k_used+1].pop_back();
+  }
+}
+
+void kpmp_poda_total(vector<vector<float> > &adym, vector<list<int> > &partition, 
+    vector<list<int> > &min_partition, int node, int k, int max_k_used, 
+    float total_weight, float &min_weight)
+{
+  // check if has added all nodes
+  if (node == adym.size()) {
+    if (total_weight < min_weight) {
+      min_partition = partition;
+      min_weight = total_weight;
+    }
+    return;
+  }
+  // because max_k_used starts at 0
+  if (adym.size()-node == (k-1)-max_k_used) {
+    for (int i = 0; i < (k-1)-max_k_used; ++i) {
+      partition[max_k_used+i+1].push_back(node+i);
+    }
+    kpmp_poda_pesada(adym, partition, min_partition, adym.size(), k, k-1, 
+        total_weight, min_weight);
+    return;
+  }
+
+  // poda pesada
+  if (max_k_used == k-1) {
+    int min_weight_of_adding_rest = 0;
+    for (int i = node; i < adym.size(); i++) {
+      float min_weight_in_subset = numeric_limits<float>::max();
+      for (int j = 0; j < k; j++) {
+        float current_weight = weight_in_subset(i, partition[j], adym);
+        if (current_weight < min_weight_in_subset) {
+          min_weight_in_subset = current_weight;
+        }
+      }
+      min_weight_of_adding_rest += min_weight_in_subset;
+    }
+
+    if (min_weight_of_adding_rest+total_weight >= min_weight) {
+      return;
+    }
+  }
+
+  // try adding it to every non-empty subset
+  for (int i = 0; i <= max_k_used; i++) {
+    float added_weight = weight_in_subset(node, partition[i], adym);
+    if (total_weight+added_weight < min_weight) {
+      partition[i].push_back(node);
+      kpmp_peso_minimo(adym, partition, min_partition, node+1, k, max_k_used, 
+          total_weight+added_weight, min_weight);
+      partition[i].pop_back();
+    }
+  }
+
+  if (max_k_used < k-1) {
+    // try adding new partition
+    partition[max_k_used+1].push_back(node);
+    kpmp_poda_pesada(adym, partition, min_partition, node+1, k, max_k_used+1, 
         total_weight, min_weight);
     partition[max_k_used+1].pop_back();
   }
